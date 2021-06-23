b54bb2137fc1554b684e360c8e9e0d462a92ca52
NrkBeats <simon.nnoruka@viacesi.fr>
2021-06-23 17:51:02 +0200

    Paramétrage Inscription en MVC

--- controllers/AjouterPersonneController.php
+++ controllers/AjouterPersonneController.php
@@ -1,16 +1,28 @@
 <?php
 
     require_once '../models/PersonneModel.php';
+    require_once '../View/InscriptionView.php';
 
-    $nom = $_POST["nom"];
-    $prenom = $_POST["prenom"];
-    $login = $_POST["login"];
-    $password = $_POST["pass"];
-    $role = 0;
+    if (isset($_POST["nom"])){
 
+        $nom = $_POST["nom"];
+        $prenom = $_POST["prenom"];
+        $login = $_POST["login"];
+        $password = $_POST["pass"];
+        $role = 0;
     
-    $personne = new Personne;
-    $tabpersonne = $personne->createPersonne($nom,$prenom,$login,$password,$role);
+        
+        $personne = new Personne;
+        $tabpersonne = $personne->createPersonne($nom,$prenom,$login,$password,$role);
+        
+    }
+
+    else{
+
+        echo "bienvenue";
+    }
+
+
 
 ?>
 
